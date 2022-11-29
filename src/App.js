3b8d16967acfab432d2d9f4dbb4ce5d6e8ebe134
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2022-11-29 17:01:31 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+
 function App() {
-  return <h3>cocktail api</h3>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
