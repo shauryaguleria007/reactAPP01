9598e52fecf7f074979095e7130ada4e0ba1f163
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2022-11-29 17:29:08 +0530

    s

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
-}
+    <section className="error-page section">
+      <div className="error-container">
+        oops! page not found.
+        <p>
+          <Link to="/">
+            <h1>Home page</h1> 
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Error
+export default Error;
